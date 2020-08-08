90f9bc7702ba3c436efee4296c0fdc98889221a9
Gihun Ham <progh2@gmail.com>
2020-08-08 16:42:43 +0900

    whitenoise middleware 오타수정

--- myapi/settings.py
+++ myapi/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.moddleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myapi.urls'
